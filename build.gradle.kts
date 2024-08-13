dd31c2b41697c9a9912903e6ff2765b37a3f30a1
Anston Sorensen <ansorensen1118@gmail.com>
2024-08-13 00:26:09 -0500

    Update MC version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
 }
 
 val targetJavaVersion = 17
