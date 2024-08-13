1000ab79fd9c3f21794997a6ddecab79c5b59134
Anston Sorensen <ansorensen1118@gmail.com>
2024-08-13 14:38:20 -0500

    Make BanGUI work smoother

--- src/main/java/com/acclash/crucials/events/BanInventoryListener.java
+++ src/main/java/com/acclash/crucials/events/BanInventoryListener.java
@@ -19,24 +19,15 @@ public class BanInventoryListener implements Listener {
         if (e.getView().getTitle().equalsIgnoreCase(ChatColor.BLUE + "Player List")) {
             e.setCancelled(true);
             if (e.getCurrentItem().getType() == Material.PLAYER_HEAD) {
-                Player whotounban = player.getServer().getPlayerExact(ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName()));
-                if (whotounban.isBanned()) {
-                    BanMenuUtils.openUnBanMenu(player, whotounban);
-                    if (e.getCurrentItem().getType() == Material.BARRIER) {
-
-                        BanMenuUtils.openBanMenu(player);
-
-                    } else if (e.getCurrentItem().getType() == Material.COCOA_BEANS) {
-                        String name = ChatColor.stripColor(e.getClickedInventory().getItem(14).getItemMeta().getDisplayName());
-                        Bukkit.getBanList(BanList.Type.NAME).pardon(name);
-                        player.sendMessage("Unbanned " + name + ".");
-                        player.sendMessage("fart fart fart");
-                    }
+                Player name = player.getServer().getPlayerExact(ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName()));
+                if (name.isBanned()) {
+                    BanMenuUtils.openUnBanMenu(player, name);
                 } else {
-                    BanMenuUtils.openConfirmBanMenu(player, whotounban);
+                    BanMenuUtils.openConfirmBanMenu(player, name);
                 }
             }
         } else if (e.getView().getTitle().equalsIgnoreCase(ChatColor.RED + "Ban this player?")) {
+            e.setCancelled(true);
 
             if (e.getCurrentItem().getType() == Material.BARRIER) {
 
@@ -45,10 +36,26 @@ public class BanInventoryListener implements Listener {
             } else if (e.getCurrentItem().getType() == Material.COCOA_BEANS) {
                 String name = ChatColor.stripColor(e.getClickedInventory().getItem(14).getItemMeta().getDisplayName());
                 player.getServer().getBanList(BanList.Type.NAME).addBan(ChatColor.stripColor(name), "You are banned for unacceptable behavior.", null, null);
-                player.sendMessage("Banned " + name + ".");
-            }
-        }
+                player.sendMessage("Banned " + name);
 
+                BanMenuUtils.openBanMenu(player);
+            }
+        } else if (e.getView().getTitle().equalsIgnoreCase(ChatColor.RED + "Unban this player?")) {
+            e.setCancelled(true);
+
+            if (e.getCurrentItem().getType() == Material.BARRIER) {
+
+                BanMenuUtils.openBanMenu(player);
+
+            } else if (e.getCurrentItem().getType() == Material.COCOA_BEANS) {
+                String name = ChatColor.stripColor(e.getClickedInventory().getItem(14).getItemMeta().getDisplayName());
+                Bukkit.getBanList(BanList.Type.NAME).pardon(name);
+                player.sendMessage("Unbanned " + name);
+
+                BanMenuUtils.openBanMenu(player);
+            }
+
+        }
     }
 
 }
